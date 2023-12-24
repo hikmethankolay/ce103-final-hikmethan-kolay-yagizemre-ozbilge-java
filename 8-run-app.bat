4d4674986a33bda31e9249bc2671c3762c189fb2
hikmethankolay <hikmethan.kolay@gmail.com>
2023-12-24 12:13:17 +0300

    .bat file fix

--- 8-run-app.bat
+++ 8-run-app.bat
@@ -3,7 +3,7 @@
 @cd /d "%~dp0"
 
 echo Running Application
-java -jar car_maintenance_app/target/car_maintenance_app-1.0-SNAPSHOT.jar
+java -jar car_maintenance_app/target/car_maintenance-1.0-SNAPSHOT.jar
 
 echo Operation Completed!
 pause
